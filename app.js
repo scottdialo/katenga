e55c483f7713493b4082ddc9a51678a2008c0f97
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-08-31 18:21:55 -0700

    made etter

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const ejs = require("ejs");
 const app = express();
 const _ = require("lodash");
 
-///multer code for storing and handling upload image
+///multer code for storing and handling upload images
 const multer = require("multer");
 const path = require("path");
 const RealEstateDb = require("./RealEstateDb");
