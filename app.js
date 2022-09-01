f4401ee9c3db8fba9fd74704cbd22c8d6f1ecffe
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-08-31 22:51:38 -0700

    port const created

--- app.js
+++ app.js
@@ -363,6 +363,7 @@ app.get("/electronicsPosts/:electronicPostName", function (req, res) {
 });
 
 //server route
-app.listen(process.env.PORT || 3000, function () {
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
